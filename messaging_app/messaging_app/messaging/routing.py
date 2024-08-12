a4a8f122fbaefc4c2eb55480c4234d2a197c7dba
mmesg <59834146+mmesg@users.noreply.github.com>
2024-08-12 21:41:40 +0200

    Delete messaging_app directory

--- messaging_app/messaging_app/messaging/routing.py
+++
@@ -1,8 +0,0 @@
-# messaging/routing.py
-
-from django.urls import path
-from .consumers import ChatConsumer
-
-websocket_urlpatterns = [
-    path('ws/chat/<str:chat_name>/', ChatConsumer.as_asgi()),
-]
